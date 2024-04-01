ea84b35c20b39e1af178a9b68714b791dbdddb1a
S.P. Mohanty <spmohanty91@gmail.com>
2024-04-01 13:08:03 +0000

    remove dynamic character limit

--- models/dummy_model.py
+++ models/dummy_model.py
@@ -1,18 +1,18 @@
 from typing import List
 
+
 class DummyModel:
     def __init__(self):
         """ Initialize your models here """
         pass
 
-    def generate_answer(self, query: str, search_results: List[str], character_limit: int) -> str:
+    def generate_answer(self, query: str, search_results: List[str]) -> str:
         """
         You will be provided with a query and the corresponding pre-cached search results for the query
         
         Inputs - 
             query - String representing the input query
             search_results - List of strings, each comes from scraped HTML text of the search query
-            character_limit - A maximum character limit for the answer (can vary per query)
         Returns - 
             string response - Your answer in plain text, should be limited to the character limit, 
                               Any longer responses will be trimmed to meet the character limit
