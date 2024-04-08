1ac6a32945e4293f88dd305b7a26836825039bd7
mohanty <mohanty@aicrowd.com>
2024-04-08 23:03:26 +0000

    Update user_config.py

--- models/user_config.py
+++ models/user_config.py
@@ -3,7 +3,7 @@ from models.dummy_model import DummyModel
 UserModel = DummyModel
 
 # Uncomment the lines below to use the Vanilla LLAMA baseline
-# from models.vanilla_llama import ChatModel 
+# from models.vanilla_llama_baseline import ChatModel 
 # UserModel = ChatModel
 
 
