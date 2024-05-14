a0ce3feb736b30ccf54fadfaa7f664d668bc628b
EnesOzeren <enesozeren7@gmail.com>
2024-05-14 21:08:45 +0200

    aicrowd.hson and user_config.py updated

--- models/user_config.py
+++ models/user_config.py
@@ -1,6 +1,6 @@
-from models.dummy_model import DummyModel
+# from models.dummy_model import DummyModel
 
-UserModel = DummyModel
+# UserModel = DummyModel
 
 # Uncomment the lines below to use the Vanilla LLAMA baseline
 # from models.vanilla_llama_baseline import ChatModel 
@@ -8,5 +8,5 @@ UserModel = DummyModel
 
 
 # Uncomment the lines below to use the RAG LLAMA baseline
-# from models.rag_llama_baseline import RAGModel
-# UserModel = RAGModel
+from models.rag_llama_baseline import RAGModel
+UserModel = RAGModel
