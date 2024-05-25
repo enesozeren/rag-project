ce8eb6c2ec5912786741f7f29782d1c779700021
EnesOzeren <enesozeren7@gmail.com>
2024-05-25 12:27:48 +0200

    user config updated for ragmodel

--- models/user_config.py
+++ models/user_config.py
@@ -4,11 +4,11 @@
 # UserModel = DummyModel
 
 # Uncomment the lines below to use the Vanilla LLAMA baseline
-from models.vanilla_llama_baseline import InstructModel
+# from models.vanilla_llama_baseline import InstructModel
 
-UserModel = InstructModel
+# UserModel = InstructModel
 
 
 # Uncomment the lines below to use the RAG LLAMA baseline
-# from models.rag_llama_baseline import RAGModel
-# UserModel = RAGModel
+from models.rag_llama_baseline import RAGModel
+UserModel = RAGModel
