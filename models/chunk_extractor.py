b5f63d41ef2d2f75098efff7674a7acd3718894f
enes_ozeren <enozeren@gmail.com>
2024-07-21 12:20:27 +0200

    chunking algorithm updated to paragraph v2 alg

--- models/chunk_extractor.py
+++ models/chunk_extractor.py
@@ -4,13 +4,128 @@ from collections import defaultdict
 from bs4 import BeautifulSoup
 from blingfire import text_to_sentences_and_offsets
 from models.utils import load_config
-
+import justext
 
 class ChunkExtractor:
     def __init__(self, config_path="config/default_config.yaml"):
         # Load configuration
         self.CONFIG = load_config(config_path)
 
+    def _sentence_chunking(self, html_source):
+        '''
+        Extracts sentences chunks from given HTML source.
+        Input:
+            html_source (str): HTML content from which to extract text.
+        Output:
+            List[str]: A list of sentences extracted from the HTML content.
+        '''
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html_source, "lxml")
+        text = soup.get_text(
+            " ", strip=True
+        )  # Use space as a separator, strip whitespaces
+
+        if not text:
+            # Return a list with empty string when no text is extracted
+            return [""]
+
+        # Extract offsets of sentences from the text
+        _, offsets = text_to_sentences_and_offsets(text)
+
+        # Initialize a list to store sentences
+        sentence_chunks = []
+
+        # Iterate through the list of offsets and extract sentences
+        for start, end in offsets:
+            # Extract the sentence and limit its length
+            sentence = text[start:end][: self.CONFIG['RagSystemParams']['MAX_CONTEXT_SENTENCE_LENGTH']]
+            sentence = text[start:end]
+            sentence_chunks.append(sentence)
+
+        return sentence_chunks
+    
+    def _paragraph_chunking(self, html_source):
+        '''
+        Extracts paragraph chunks from given HTML source.
+        Input:
+            html_source (str): HTML content from which to extract text.
+        Output:
+            List[str]: A list of paragraphs extracted from the HTML content.
+        '''
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html_source, "lxml")
+        text = soup.get_text(
+            " ", strip=False
+        )  # Use space as a separator, strip whitespaces
+        
+        # Split the input text by newline characters
+        parts = text.split('\n')
+        
+        # Filter out the empty strings and whitespace-only strings
+        paragraph_chunks = [part.strip() for part in parts if part.strip()]
+        # Limit the paragraph length
+        paragraph_chunks = [paragraph[: self.CONFIG['RagSystemParams']['MAX_CONTEXT_SENTENCE_LENGTH']] 
+                            for paragraph in paragraph_chunks]
+        
+        return paragraph_chunks
+    
+    def _sentence_chunking_without_boilerplate(self, html_source):
+        '''
+        Extracts sentences chunks after removing the boilerplate 
+        (such as navigation links, headers, and footers) from given HTML source.
+        Input:
+            html_source (str): HTML content from which to extract text.
+        Output:
+            List[str]: A list of sentences extracted from the HTML content.
+        '''
+        # Use justext to remove boilerplate and extract meaningful text
+        paragraphs = justext.justext(html_source, 
+                                     justext.get_stoplist("English"),
+                                     max_link_density=1)
+        paragraph_chunks = [paragraph.text for paragraph in paragraphs if not paragraph.is_boilerplate]
+        if paragraph_chunks != []:
+            # Split paragraphs into sentences
+            sentence_chunks = []
+            for paragraph in paragraph_chunks:
+                # Extract offsets of sentences from the text
+                _, offsets = text_to_sentences_and_offsets(paragraph)
+
+                # Iterate through the list of offsets and extract sentences
+                for start, end in offsets:
+                    # Extract the sentence and limit its length
+                    sentence = paragraph[start:end][: self.CONFIG['RagSystemParams']['MAX_CONTEXT_SENTENCE_LENGTH']]
+                    sentence = paragraph[start:end]
+                    sentence_chunks.append(sentence)
+
+        # If no paragraphs are found use the old sentence chunking method
+        else:
+            sentence_chunks = self._sentence_chunking(html_source)    
+        
+        return sentence_chunks
+    
+    def _paragraph_chunking_without_boilerplate(self, html_source):
+        '''
+        Extracts paragraphs chunks after removing the boilerplate 
+        (such as navigation links, headers, and footers) from given HTML source.
+        Input:
+            html_source (str): HTML content from which to extract text.
+        Output:
+            List[str]: A list of paragraphs extracted from the HTML content.
+        '''
+        # Use justext to remove boilerplate and extract meaningful text
+        paragraphs = justext.justext(html_source, 
+                                     justext.get_stoplist("English"), 
+                                     max_link_density=1)
+        paragraph_chunks = [paragraph.text for paragraph in paragraphs if not paragraph.is_boilerplate]
+        # Limit the paragraph length
+        paragraph_chunks = [paragraph[: self.CONFIG['RagSystemParams']['MAX_CONTEXT_SENTENCE_LENGTH']] 
+                            for paragraph in paragraph_chunks]
+        
+        if paragraph_chunks == []:
+            paragraph_chunks = self._paragraph_chunking(html_source)         
+        
+        return paragraph_chunks
+    
     @ray.remote
     def _extract_chunks(self, interaction_id, html_source):
         """
@@ -27,29 +142,16 @@ class ChunkExtractor:
         Returns:
             Tuple[str, List[str]]: A tuple containing the interaction ID and a list of sentences extracted from the HTML content.
         """
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(html_source, "lxml")
-        text = soup.get_text(
-            " ", strip=True
-        )  # Use space as a separator, strip whitespaces
-
-        if not text:
+        if html_source == "":
             # Return a list with empty string when no text is extracted
             return interaction_id, [""]
-
-        # Extract offsets of sentences from the text
-        _, offsets = text_to_sentences_and_offsets(text)
-
-        # Initialize a list to store sentences
-        chunks = []
-
-        # Iterate through the list of offsets and extract sentences
-        for start, end in offsets:
-            # Extract the sentence and limit its length
-            sentence = text[start:end][: self.CONFIG['RagSystemParams']['MAX_CONTEXT_SENTENCE_LENGTH']]
-            chunks.append(sentence)
+        
+        # Extract chunks from the HTML source
+        chunks = self._paragraph_chunking_without_boilerplate(html_source)
 
         return interaction_id, chunks
+
+
 
     def extract_chunks(self, batch_interaction_ids, batch_search_results):
         """
